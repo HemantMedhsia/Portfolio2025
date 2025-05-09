6f32d23bb5184810f616226361bce828866fc74e
Hemant Medhsia <hemantmedhsia@gmail.com>
2025-05-09 13:16:16 +0530

    initialcommit

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -10,8 +10,8 @@ const Header: React.FC = () => {
     return (
         <motion.div>
             <motion.div
-                className="fixed top-10 left-1/2 transform -translate-x-1/2 w-full max-w-4xl h-16 px-6 flex items-center justify-between 
-                rounded-3xl backdrop-blur-3xl bg-[#15161C]/50 border-2 border-[#2A2B37] z-50 overflow-hidden"
+                className="fixed top-4 md:top-10 left-1/2 transform -translate-x-1/2 w-full max-w-4xl h-16 px-4 md:px-6 flex items-center justify-between 
+                md:rounded-3xl backdrop-blur-3xl bg-[#15161C]/50 border-2 border-[#2A2B37] z-50 overflow-hidden"
             >
                 <LogoSection />
                 <SocialIcons />
--- src/components/Header/SocialIcons.tsx
+++ src/components/Header/SocialIcons.tsx
@@ -1,6 +1,7 @@
 import { ImGithub, ImLinkedin2 } from "react-icons/im";
 import { IoLogoInstagram } from "react-icons/io5";
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { CiMenuFries } from "react-icons/ci";
 import { useMusic } from "../../context/MusicContext";
 
 const SocialIcons: React.FC = () => {
@@ -13,12 +14,13 @@ const SocialIcons: React.FC = () => {
                         src="https://lottie.host/a82cdb51-bea7-4538-9509-19cb323a5e2c/2wPmYKSeVZ.lottie"
                         loop
                         autoplay
-                        className="w-50 h-10 cursor-pointer absolute -top-3 -left-34"
+                        className="w-50 h-10 cursor-pointer absolute -top-3 -left-34 hidden md:block"
                     />
                 )}
-                <a href="https://github.com/hemantmedhsia" target="_blank" rel="noopener noreferrer" className="hover:text-teal-300 cursor-pointer"><ImGithub size={24} /></a>
-                <a href="https://www.linkedin.com/in/hemantmedhsia/" target="_blank" rel="noopener noreferrer" className="hover:text-teal-300 cursor-pointer"><ImLinkedin2 size={24} /></a>
-                <a href="https://www.instagram.com/hemant_____g/" target="_blank" rel="noopener noreferrer" className="hover:text-teal-300 cursor-pointer"><IoLogoInstagram size={24} /></a>
+                <a href="https://github.com/hemantmedhsia" target="_blank" rel="noopener noreferrer" className="hover:text-teal-300 cursor-pointer hidden md:block"><ImGithub size={24} /></a>
+                <a href="https://www.linkedin.com/in/hemantmedhsia/" target="_blank" rel="noopener noreferrer" className="hover:text-teal-300 cursor-pointer hidden md:block"><ImLinkedin2 size={24} /></a>
+                <a href="https://www.instagram.com/hemant_____g/" target="_blank" rel="noopener noreferrer" className="hover:text-teal-300 cursor-pointer hidden md:block"><IoLogoInstagram size={24} /></a>
+                <div className="hover:text-teal-300 cursor-pointer block md:hidden"><CiMenuFries size={24} /></div>
             </div>
         </>
     )
